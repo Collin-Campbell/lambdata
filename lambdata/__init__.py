bfa14ded9c55ebcd8c781aad88903c981ac9d52f
Collin-Campbell <70334677+Collin-Campbell@users.noreply.github.com>
2020-10-29 12:29:03 -0400

    Update __init__.py

--- lambdata/__init__.py
+++ lambdata/__init__.py
@@ -23,25 +23,3 @@ class DataFrame(pd.DataFrame):
         """splits target from df"""
         self.y = self[str(target_col)]
         self.X = self.drop(columns=str(target_col))
-=======
-# accessing libraries through pipenv
-import pandas as pd
-import numpy as np
-
-
-
-def df_missing_values(df):
-    """counts missing values"""
-
-    return df.isnull().sum()
-
-
-
-def target_split(df, target_str):
-    """splits target from df"""
-
-    y = df[target_str]
-    X = df.drop(columns=target_str)
-
-    return y, X
->>>>>>> 4ef46b43cfe76cd92cc91152d7835e93e6befe06
